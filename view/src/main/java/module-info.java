622bebca4b0d2a7a5a9c5f6cd64c466e1b64ffae
Markus Bodenberger <ujwny@student.kit.edu>
2024-01-16 17:04:50 +0100

    Added: ViewFacade method stubs and attributes

--- view/src/main/java/module-info.java
+++ view/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module edu.kit.ifv.trafficspvisualizer.view {
     requires javafx.controls;
+    requires edu.kit.ifv.trafficspvisualizer.model;
+
+
+
 
     exports edu.kit.ifv.trafficspvisualizer.view;
     exports edu.kit.ifv.trafficspvisualizer.view.window;
