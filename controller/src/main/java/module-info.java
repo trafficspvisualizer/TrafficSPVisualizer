17ae5db5b96a69b378444fa19a6388296b39326c
Fabian Kühn <ughhz@student.kit.edu>
2024-01-17 16:18:02 +0100

    added needed dependencies.

--- controller/src/main/java/module-info.java
+++ controller/src/main/java/module-info.java
@@ -3,4 +3,7 @@ module edu.kit.ifv.trafficspvisualizer.controller {
 
     requires edu.kit.ifv.trafficspvisualizer.view;
     requires edu.kit.ifv.trafficspvisualizer.model;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires edu.kit.ifv.trafficspvisualizer.util.project;
 }
