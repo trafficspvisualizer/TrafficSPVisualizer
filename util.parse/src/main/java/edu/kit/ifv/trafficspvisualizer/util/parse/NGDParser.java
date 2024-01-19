73147b98fa89dfb76a2b8da9697e7b6e861503c0
Hans Wormuth <urnyq@student.kit.edu>
2024-01-19 17:52:56 +0100

    read data from ngd file into 2D array

--- util.parse/src/main/java/edu/kit/ifv/trafficspvisualizer/util/parse/NGDParser.java
+++ util.parse/src/main/java/edu/kit/ifv/trafficspvisualizer/util/parse/NGDParser.java
@@ -1,12 +1,48 @@
 package edu.kit.ifv.trafficspvisualizer.util.parse;
 
 import edu.kit.ifv.trafficspvisualizer.model.DataObject;
+import edu.kit.ifv.trafficspvisualizer.model.SituationData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class NGDParser extends Parser{
     @Override
     public DataObject parse(File file) {
+        DataObject dataObject = new DataObject();
+        SituationData[] situations = new SituationData[1];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            long lineCount = br.lines().count();
+            String[][] initialData = new String[(int) lineCount][];
+            br = new BufferedReader(new FileReader(file));
+
+            int row = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("|")) {
+                    break;
+                }
+                String[] values = line.split("\t");
+                initialData[row] = values;
+                row++;
+            }
+            String[][] data = new String[row][];
+            System.arraycopy(initialData, 0, data, 0, row);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found");
+
+        } catch (IOException e) {
+            System.out.println("Exception caught");
+        }
+
+        HashMap hashMap = new HashMap<>();
         return null;
     }
 }
+
