e4727afa17f91f5aa322278393f878393a86be9c
Fabio Freund <fabio@freund.io>
2024-01-22 14:36:01 +0100

    Implemented DataObject

--- model/src/main/java/edu/kit/ifv/trafficspvisualizer/model/DataObject.java
+++ model/src/main/java/edu/kit/ifv/trafficspvisualizer/model/DataObject.java
@@ -1,9 +1,27 @@
 package edu.kit.ifv.trafficspvisualizer.model;
 
-public class DataObject {
+import java.util.Set;
 
-    public SituationData getSituationData(int index) {
-        //TODO
-        return null;
+public class DataObject {
+    private final SituationData[] situationData;
+
+    public DataObject(SituationData[] situationData) {
+        this.situationData = situationData;
+    }
+
+    public int getBlockNumber(int situationIndex) {
+        return this.situationData[situationIndex].getBlockNumber();
+    }
+
+    public double getAttributeValue(int situationIndex, String choiceName, String attributeName) {
+        return this.situationData[situationIndex].getChoiceData(choiceName).getValue(attributeName);
+    }
+
+    public Set<String> getChoiceNames(int situationIndex) {
+        return this.situationData[situationIndex].getNames();
+    }
+
+    public Set<String> getAttributeNames(int situationIndex, String choiceName) {
+        return this.situationData[situationIndex].getChoiceData(choiceName).getNames();
     }
 }
