015fac40472daa83577a3b596ac16aa888cbd54c
Jakub Suliga <uhtfz@student.kit.edu>
2024-01-22 14:07:53 +0100

    Added to ImageToBufferedImageConverter Exceptions and fixed bugs in SVGToBufferedImageConverter and added a test class for SVGToBufferedImageConverter

---
+++ util.image/src/test/java/edu/kit/ifv/trafficspvisualizer/util/image/SVGToBufferedImageConverterTest.java
@@ -0,0 +1,13 @@
+package edu.kit.ifv.trafficspvisualizer.util.image;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SVGToBufferedImageConverterTest {
+
+    @Test
+    void convert() {
+
+    }
+}
