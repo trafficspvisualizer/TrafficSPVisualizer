eaf4763cf62b8df23f46d761264eebf392a2cba8
Hans Wormuth <urnyq@student.kit.edu>
2024-01-25 11:48:05 +0100

    Added NGDParserBlockNumberTest.java

---
+++ util.parse/src/test/java/edu/kit/ifv/trafficspvisualizer/util/parse/NGDParserBlockNumberTest.java
@@ -0,0 +1,21 @@
+package edu.kit.ifv.trafficspvisualizer.util.parse;
+
+import edu.kit.ifv.trafficspvisualizer.model.DataObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NGDParserBlockNumberTest {
+    @Test
+    public  void testBlockNumber() {
+        //ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(getClass().getResource("/BeispielTest.ngd").getFile());
+        NGDParser ngdParser = new NGDParser();
+        DataObject dataObject = ngdParser.parse(file);
+        assertEquals(16, dataObject.getSituationCount());
+        assertEquals(1, dataObject.getBlockNumber(15));
+        assertEquals(3, dataObject.getBlockNumber(0));
+    }
+}
