9a088ffdf03e878b4e7e7dcffdb034d5bfeb99c3
Kuba3105 <66252366+Kuba3105@users.noreply.github.com>
2024-01-28 16:23:19 +0100

    added if a directory do not exsists, it creates the directory

--- util.export/src/main/java/edu/kit/ifv/trafficspvisualizer/util/export/ImageExporter.java
+++ util.export/src/main/java/edu/kit/ifv/trafficspvisualizer/util/export/ImageExporter.java
@@ -4,19 +4,26 @@ import edu.kit.ifv.trafficspvisualizer.util.image.ChoiceOptionImage;
 
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class ImageExporter extends Exporter{
 
     @Override
     public void export(ChoiceOptionImage[] images, File file) throws IOException {
         for (ChoiceOptionImage image: images) {
-            StringBuilder path = new StringBuilder(file.getPath() + "\\");
+            StringBuilder path = new StringBuilder(file.getPath() + File.separator);
             for (String info: image.getInfos()) {
                 path.append("#c_").append(info).append("#");
             }
             File imageFile = new File(path + ".png");
-            ImageIO.write(image.getImage(), ".png", imageFile);
+            if (!imageFile.getParentFile().exists()) {
+                imageFile.getParentFile().mkdirs();
+            }
+            try (OutputStream os = new FileOutputStream(imageFile)) {
+                ImageIO.write(image.getImage(), "png", os);
+            }
         }
     }
 }
