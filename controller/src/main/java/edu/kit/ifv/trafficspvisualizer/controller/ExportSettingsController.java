33586db59ad4d8999b0c68ebed76699ab841f4c4
Fabian Kühn <ughhz@student.kit.edu>
2024-01-28 13:04:06 +0100

    Added method implementation to ExportSettingsController.

--- controller/src/main/java/edu/kit/ifv/trafficspvisualizer/controller/ExportSettingsController.java
+++ controller/src/main/java/edu/kit/ifv/trafficspvisualizer/controller/ExportSettingsController.java
@@ -1,5 +1,6 @@
 package edu.kit.ifv.trafficspvisualizer.controller;
 
+import edu.kit.ifv.trafficspvisualizer.model.ExportSettings;
 import edu.kit.ifv.trafficspvisualizer.model.ExportType;
 import edu.kit.ifv.trafficspvisualizer.model.FileFormat;
 
@@ -12,15 +13,32 @@ public class ExportSettingsController {
         this.controllerFacade = controllerFacade;
     }
 
-    public void actionOnExportFolderButton(File exportFolder){
-
+    public void actionOnExportFolderButton(){
+        File selectedFile = controllerFacade.getViewFacade().getExportSettingsStage().showFileChooserDialog();
+        controllerFacade.getViewFacade().getExportSettingsStage().setExportFolderPath(selectedFile);
     }
 
-    public void actionOnSaveButton(FileFormat fileFormat, int height, int width, File exportFolder, ExportType exportType){
+    public void actionOnSaveButton(){
 
+        // scraping data from view
+        //TODO: Placeholder methods
+        FileFormat fileFormat = controllerFacade.getViewFacade().getExportSettingsStage().getFileFormat();
+        int height = controllerFacade.getViewFacade().getExportSettingsStage().getHeightTEST();
+        int width = controllerFacade.getViewFacade().getExportSettingsStage().getWidthTEST();
+        File exportFolder exportFolder = controllerFacade.getViewFacade().getExportSettingsStage().getExportFolder();
+        ExportType exportType = controllerFacade.getViewFacade().getExportSettingsStage().getExportType();
+
+        // checking validity of scrape data
+        //TODO: checking validity of scrape data
+
+        // setting new export settings in model
+        ExportSettings exportSettings = new ExportSettings(height, width, exportFolder, fileFormat, exportType);
+        controllerFacade.getProject().setExportSettings(exportSettings);
     }
 
     public void actionOnCancelButton(){
-
+        controllerFacade.getViewFacade().getExportSettingsStage().close();
+        controllerFacade.getViewFacade().setExportSettingsStage(null);
+        controllerFacade.deleteExportSettingsController();
     }
 }
