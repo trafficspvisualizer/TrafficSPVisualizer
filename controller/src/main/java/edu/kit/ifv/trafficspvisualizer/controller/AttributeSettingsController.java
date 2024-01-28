f10fdcb671f3b9304c1ec8d3c26d382a9f516e1d
Fabian Kühn <ughhz@student.kit.edu>
2024-01-28 13:30:24 +0100

    Added method implementations to AttributeSettingsController.

--- controller/src/main/java/edu/kit/ifv/trafficspvisualizer/controller/AttributeSettingsController.java
+++ controller/src/main/java/edu/kit/ifv/trafficspvisualizer/controller/AttributeSettingsController.java
@@ -1,5 +1,6 @@
 package edu.kit.ifv.trafficspvisualizer.controller;
 
+import edu.kit.ifv.trafficspvisualizer.model.Attribute;
 import edu.kit.ifv.trafficspvisualizer.model.Icon;
 import javafx.scene.image.Image;
 
@@ -16,9 +17,41 @@ public class AttributeSettingsController implements IconDisplayingController {
         controllerFacade.createIconSelectionController(this, attributeIndex);
     }
 
-    public void actionOnSaveButton(String name, Image icon, String prefix, String suffix, boolean isAlwaysVisible){
-        //TODO: keep attributeIndex in mind, check if existing
-        //TODO: Attribute constructor missing
+    public void actionOnSaveButton(){
+        //scraping data from view
+        //TODO: placeholder methods
+        String name = controllerFacade.getViewFacade().getAttributeSettingsStage().getName();
+        Icon icon = controllerFacade.getViewFacade().getAttributeSettingsStage().getIcon();
+        String prefix = controllerFacade.getViewFacade().getAttributeSettingsStage().getPrefix();
+        String suffix = controllerFacade.getViewFacade().getAttributeSettingsStage().getSuffix();
+        boolean isPermanentlyVisible = controllerFacade.getViewFacade().
+                                            getAttributeSettingsStage().getPermanentlyVisible();
+        int decimalPlaces = controllerFacade.getViewFacade().getAttributeSettingsStage().getDecimalPlaces();
+
+
+        //check if editing existing Attribute or adding new one
+        // if attributeIndex is index of attribute list
+        if (attributeIndex < controllerFacade.getProject().getAttributes().size()) {
+            //edit existing Attribute
+            // type casting should be no problem cause index is given by AttributeController which ensures
+            // only indexes of non-separator-line Attributes are given
+            Attribute existingAttribute = (Attribute) controllerFacade.getProject().getAttributes().get(attributeIndex);
+            existingAttribute.setName(name);
+            //TODO: icon should be changed to type Icon instead of Image
+            existingAttribute.setIcon(icon);
+            existingAttribute.setPrefix(prefix);
+            existingAttribute.setSuffix(suffix);
+            existingAttribute.setPermanentlyVisible(isPermanentlyVisible);
+            existingAttribute.setDecimalPlaces(decimalPlaces);
+
+
+        // if attributeIndex is out of bounds, index is given by AttributeController
+        } else {
+            //create new attribute
+            //TODO: Attribute constructor missing
+            Attribute newAttribute = new Attribute(name, icon, prefix, suffix, isAlwaysVisible, decimalPlaces);
+        }
+
     }
 
     public void actionOnCancelButton(){
@@ -29,7 +62,8 @@ public class AttributeSettingsController implements IconDisplayingController {
 
     @Override
     public void updateIcon(Icon icon, int index){
-
+        //TODO: setIconPreview should accept Type Icon
+        controllerFacade.getViewFacade().getAttributeSettingsStage().setIconPreview(icon);
     }
 
     public int getAttributeIndex() {
