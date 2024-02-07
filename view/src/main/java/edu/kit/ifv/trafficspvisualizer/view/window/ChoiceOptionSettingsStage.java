4db9c03149669903c22c40e6074d61c2fcedb475
Markus Bodenberger <ujwny@student.kit.edu>
2024-02-07 18:43:22 +0100

    Changed: Active CheckBox from AttributeSettingsStage to AttributeStage Added: Remove Attribute Button to AttributeStage

--- view/src/main/java/edu/kit/ifv/trafficspvisualizer/view/window/ChoiceOptionSettingsStage.java
+++ view/src/main/java/edu/kit/ifv/trafficspvisualizer/view/window/ChoiceOptionSettingsStage.java
@@ -1,11 +1,21 @@
 package edu.kit.ifv.trafficspvisualizer.view.window;
 
 import edu.kit.ifv.trafficspvisualizer.view.ViewFacade;
+import edu.kit.ifv.trafficspvisualizer.view.data.language.LanguageStrategy;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class ChoiceOptionSettingsStage extends Stage {
 
+    private ViewFacade viewFacade;
+
+
+
     public ChoiceOptionSettingsStage(ViewFacade viewFacade, int coIndex) {
+        this.viewFacade = viewFacade;
 
     }
 
@@ -16,5 +26,17 @@ public class ChoiceOptionSettingsStage extends Stage {
 
     public void updateAttributes() {
 
+    }
+
+    // show-methods
+    public Optional<ButtonType> showRemoveRouteSectionConfirmationAlert() {
+        LanguageStrategy languageStrategy = viewFacade.getLanguageStrategy();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(languageStrategy.getRemoveRouteSectionConfirmationAlertTitle());
+        alert.setHeaderText(languageStrategy.getRemoveRouteSectionConfirmationAlertHeaderText());
+        alert.setContentText(languageStrategy.getRemoveRouteSectionConfirmationAlertContentText());
+
+        return alert.showAndWait();
     }
 }
