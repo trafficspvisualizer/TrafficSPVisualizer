e2c12b04a139a386247875041cfc4284dd4b1645
Markus Bodenberger <ujwny@student.kit.edu>
2024-02-13 00:11:35 +0100

    Added: ViewFacade JavaDoc

--- view/src/main/java/edu/kit/ifv/trafficspvisualizer/view/ViewFacade.java
+++ view/src/main/java/edu/kit/ifv/trafficspvisualizer/view/ViewFacade.java
@@ -21,7 +21,7 @@ public class ViewFacade {
 
     private LanguageStrategy languageStrategy;
 
-    private MainApplicationWindow mainApplicationWindow;
+    private final MainApplicationWindow mainApplicationWindow;
 
     private ProjectCreationStage projectCreationStage;
 
@@ -36,10 +36,12 @@ public class ViewFacade {
     private IconSelectionStage iconSelectionStage;
 
 
-
-
-
-
+    /**
+     * Creates the view facade by setting necessary attributes and creating the {@link MainApplicationWindow}.
+     *
+     * @param primaryStage Main stage in the JavaFX application and needed by the {@link MainApplicationWindow}
+     * @param project Initial project
+     */
     public ViewFacade(Stage primaryStage, Project project) {
         this.project = project;
         languageStrategy = new EnglishLanguageStrategy();
@@ -49,38 +51,83 @@ public class ViewFacade {
     // getter-Methods
 
 
+    /**
+     * Gets the {@link Project}.
+     *
+     * @return The project on which the user is currently working.
+     */
     public Project getProject() {
         return project;
     }
 
+    /**
+     * Gets the {@link LanguageStrategy}.
+     *
+     * @return The current {@link LanguageStrategy}.
+     */
     public LanguageStrategy getLanguageStrategy() {
         return languageStrategy;
     }
 
+    /**
+     * Gets the {@link MainApplicationWindow}.
+     *
+     * @return The current {@link MainApplicationWindow} displayed to the user.
+     */
     public MainApplicationWindow getMainApplicationWindow() {
         return mainApplicationWindow;
     }
 
+    /**
+     * Gets the {@link ProjectCreationStage}.
+     *
+     * @return The current {@link ProjectCreationStage} displayed to the user.
+     */
     public ProjectCreationStage getProjectCreationStage() {
         return projectCreationStage;
     }
 
+    /**
+     * Gets the {@link AttributeStage}.
+     *
+     * @return The current {@link AttributeStage} displayed to the user.
+     */
     public AttributeStage getAttributeStage() {
         return attributeStage;
     }
 
+    /**
+     * Gets the {@link AttributeSettingsStage}.
+     *
+     * @return The current {@link AttributeSettingsStage} displayed to the user.
+     */
     public AttributeSettingsStage getAttributeSettingsStage() {
         return attributeSettingsStage;
     }
 
+    /**
+     * Gets the {@link ChoiceOptionSettingsStage}.
+     *
+     * @return The current {@link ChoiceOptionSettingsStage} displayed to the user.
+     */
     public ChoiceOptionSettingsStage getChoiceOptionSettingsStage() {
         return choiceOptionSettingsStage;
     }
 
+    /**
+     * Gets the {@link ExportSettingsStage}.
+     *
+     * @return The current {@link ExportSettingsStage} displayed to the user.
+     */
     public ExportSettingsStage getExportSettingsStage() {
         return exportSettingsStage;
     }
 
+    /**
+     * Gets the {@link IconSelectionStage}.
+     *
+     * @return The current {@link IconSelectionStage} displayed to the user.
+     */
     public IconSelectionStage getIconSelectionStage() {
         return iconSelectionStage;
     }
@@ -88,34 +135,65 @@ public class ViewFacade {
 
     // setter-Methods
 
+    /**
+     * Sets the current {@link Project}.
+     *
+     * @param project The new project.
+     */
     public void setProject(Project project) {
         this.project = project;
     }
 
-    public void setMainApplicationWindow(MainApplicationWindow mainApplicationWindow) {
-        this.mainApplicationWindow = mainApplicationWindow;
-    }
-
+    /**
+     * Sets the current {@link ProjectCreationStage}.
+     *
+     * @param projectCreationStage The new {@link ProjectCreationStage} displayed to the user.
+     */
     public void setProjectCreationStage(ProjectCreationStage projectCreationStage) {
         this.projectCreationStage = projectCreationStage;
     }
 
+    /**
+     * Sets the current {@link AttributeStage}.
+     *
+     * @param attributeStage The new {@link AttributeStage} displayed to the user.
+     */
     public void setAttributeStage(AttributeStage attributeStage) {
         this.attributeStage = attributeStage;
     }
 
+    /**
+     * Sets the current {@link AttributeSettingsStage}.
+     *
+     * @param attributeSettingsStage The new {@link AttributeSettingsStage} displayed to the user.
+     */
     public void setAttributeSettingsStage(AttributeSettingsStage attributeSettingsStage) {
         this.attributeSettingsStage = attributeSettingsStage;
     }
 
+    /**
+     * Sets the current {@link ChoiceOptionSettingsStage}.
+     *
+     * @param choiceOptionSettingsStage The new {@link ChoiceOptionSettingsStage} displayed to the user.
+     */
     public void setChoiceOptionSettingsStage(ChoiceOptionSettingsStage choiceOptionSettingsStage) {
         this.choiceOptionSettingsStage = choiceOptionSettingsStage;
     }
 
+    /**
+     * Sets the current {@link ExportSettingsStage}.
+     *
+     * @param exportSettingsStage The new {@link ExportSettingsStage} displayed to the user.
+     */
     public void setExportSettingsStage(ExportSettingsStage exportSettingsStage) {
         this.exportSettingsStage = exportSettingsStage;
     }
 
+    /**
+     * Sets the current {@link IconSelectionStage}.
+     *
+     * @param iconSelectionStage The new {@link IconSelectionStage} displayed to the user.
+     */
     public void setIconSelectionStage(IconSelectionStage iconSelectionStage) {
         this.iconSelectionStage = iconSelectionStage;
     }
