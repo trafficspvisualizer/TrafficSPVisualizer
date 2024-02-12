d7d608bb5cbc80ed95e8c2b8e833eaaad60781d0
Kuba3105 <66252366+Kuba3105@users.noreply.github.com>
2024-02-12 13:03:44 +0100

    changed the loader to be more effizent

--- util.project/src/main/java/edu/kit/ifv/trafficspvisualizer/util/project/SharedConstants.java
+++ util.project/src/main/java/edu/kit/ifv/trafficspvisualizer/util/project/SharedConstants.java
@@ -1,25 +1,34 @@
 package edu.kit.ifv.trafficspvisualizer.util.project;
 
 public interface SharedConstants {
-    static final String KEY_IMAGE_HEIGHT = "imageHeight";
-    static final String KEY_IMAGE_WIDTH = "imageWidth";
-    static final String KEY_FILE_FORMAT = "fileFormat";
-    static final String KEY_EXPORT_TYPE = "exportType";
-    static final String KEY_ICON = "icon";
-    static final String KEY_CHOICE_DATA_KEY = "choiceDataKey";
-    static final String KEY_LINE_TYPE = "lineType";
-    static final String KEY_NAME = "name";
-    static final String KEY_PREFIX = "prefix";
-    static final String KEY_SUFFIX = "suffix";
-    static final String KEY_PERMANENTLY_VISIBLE = "permanentlyVisible";
-    static final String KEY_DECIMAL_PLACES = "decimalPlaces";
-    static final String KEY_CHOICE_OPTION_MAPPINGS = "choiceOptionMappings";
-    static final String KEY_NAME_CHOICE_OPTION = "name";
-    static final String KEY_ROUTE_SECTIONS = "routeSections";
-    static final String KEY_TITLE = "title";
-    static final String KEY_COLOR = "color";
-    static final String KEY_ATTRIBUTES = "attributes";
-    static final String KEY_EXPORT_SETTINGS = "exportSettings";
-    static final String KEY_LINE_SEPARATOR = "LineSeparator";
-    static final String KEY_ATTRIBUTE= "Attribute";
+
+    // Keys related to image settings
+    String KEY_IMAGE_HEIGHT = "imageHeight";
+    String KEY_IMAGE_WIDTH = "imageWidth";
+    String KEY_FILE_FORMAT = "fileFormat";
+    String KEY_EXPORT_TYPE = "exportType";
+    String KEY_ICON = "icon";
+
+    // Keys related to choice options and route sections
+    String KEY_CHOICE_DATA_KEY = "choiceDataKey";
+    String KEY_LINE_TYPE = "lineType";
+    String KEY_NAME_CHOICE_OPTION = "name";
+    String KEY_ROUTE_SECTIONS = "routeSections";
+    String KEY_CHOICE_OPTION_MAPPINGS = "choiceOptionMappings";
+
+    // Keys related to attributes
+    String KEY_NAME = "name";
+    String KEY_PREFIX = "prefix";
+    String KEY_SUFFIX = "suffix";
+    String KEY_PERMANENTLY_VISIBLE = "permanentlyVisible";
+    String KEY_DECIMAL_PLACES = "decimalPlaces";
+    String KEY_ATTRIBUTES = "attributes";
+    String KEY_ATTRIBUTE= "Attribute";
+
+    // Other keys
+    String KEY_TITLE = "title";
+    String KEY_COLOR = "color";
+    String KEY_EXPORT_SETTINGS = "exportSettings";
+    String KEY_LINE_SEPARATOR = "LineSeparator";
 }
+
