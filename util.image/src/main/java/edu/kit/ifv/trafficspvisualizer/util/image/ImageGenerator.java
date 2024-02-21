d597f498e0a1151a0de89a8b01a6221a21fbd6f6
Hans Wormuth <urnyq@student.kit.edu>
2024-02-21 20:32:33 +0100

    added: Java doc

--- util.image/src/main/java/edu/kit/ifv/trafficspvisualizer/util/image/ImageGenerator.java
+++ util.image/src/main/java/edu/kit/ifv/trafficspvisualizer/util/image/ImageGenerator.java
@@ -8,7 +8,37 @@ import edu.kit.ifv.trafficspvisualizer.model.data.InvalidDataKeyException;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
+/**
+ * The abstract class {@link ImageGenerator} is used by the {@link ImageCollectionGenerator} class to create a
+ * single image of a choice option. ImageGenerator can be extended with new classes if a different form
+ * of representation for the {@link ChoiceOption} is desired.
+ * In a new, specific ImageGenerator class, the
+ * choice options could be created in such a way that, for example, the attributes are in two lines on the right
+ * and the route sections on the left.
+ * The ImageGenerator and ImageCollectionGenerator classes have been implemented
+ * separately to ensure better extensibility.
+ */
 public abstract class ImageGenerator {
+
+    /**
+     * The createChoiceOption method creates an image of a single choice option using the given parameters.
+     * The integer value max represent the longest distance of a situation. The choiceOption, DataObject and attributes
+     * parameters provide all the important information for creating a decision option.
+     * The integer values ​​height and width determine the size of the image in pixels.
+     * The method creates the BufferedImage from the given information and returns it.
+     * The ImageGenerator and ImageCollectionGenerator classes have been implemented separately
+     * to ensure better extensibility.
+     * @param choiceOption
+     * @param dataObject
+     * @param attributes
+     * @param height
+     * @param width
+     * @param min
+     * @param max
+     * @param situationIndex
+     * @return
+     * @throws InvalidDataKeyException
+     */
 
     public abstract BufferedImage createChoiceOption(ChoiceOption choiceOption, DataObject dataObject,
                                                      List<AbstractAttribute> attributes, int height, int width, double min, double max, int situationIndex) throws InvalidDataKeyException;
