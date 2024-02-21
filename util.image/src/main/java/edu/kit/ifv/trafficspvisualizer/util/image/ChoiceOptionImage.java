3612001922e8b89d0e6ea1ed7faa6ae0f4c7f689
Hans Wormuth <urnyq@student.kit.edu>
2024-02-21 21:33:15 +0100

    added: Java doc

--- util.image/src/main/java/edu/kit/ifv/trafficspvisualizer/util/image/ChoiceOptionImage.java
+++ util.image/src/main/java/edu/kit/ifv/trafficspvisualizer/util/image/ChoiceOptionImage.java
@@ -6,16 +6,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * This class contains a bufferedImage and data, which is needed for the export.
+ */
 public class ChoiceOptionImage {
-
     private static final int SCENARIO_NUMBER_KEY = 1;
     private static final int BLOCK_NUMBER_KEY = 2;
     private static final int CHOICE_OPTION_NUMBER_KEY = 3;
     private static final int TITLE_KEY = 0;
-
     private BufferedImage image;
     private final Map<Integer, String> infos;
 
+    /**
+     * Creates a new {@link ChoiceOptionImage} object.
+     */
     public ChoiceOptionImage() {
         infos = new HashMap<>();
         infos.put(TITLE_KEY, "-1");
@@ -24,30 +28,59 @@ public class ChoiceOptionImage {
         infos.put(CHOICE_OPTION_NUMBER_KEY, "-1");
 
     }
+
+    /**
+     * Sets the bufferedImage.
+     * @param image the bufferedImage
+     */
     public void setImage(BufferedImage image) {
         this.image = image;
     }
 
+    /**
+     * Adds information needed for the export.
+     * @param info the information
+     */
     public void add(String info) {
         infos.put(infos.size(), info);
     }
 
+    /**
+     * Returns the needed information.
+     * @return the information
+     */
     public List<String> getInfos() {
         return List.copyOf(infos.values());
     }
 
+    /**
+     * Returns the bufferedImage.
+     * @return the image
+     */
     public BufferedImage getImage() {
         return image;
     }
 
+    /**
+     * The title of the choiceOption.
+     * @return the title
+     */
     public String getTitle() {
         return infos.get(TITLE_KEY);
     }
 
+    /**
+     * Returns the scenario number.
+     * @return scenario number
+     */
     public String getScenarioNumber() {
         return infos.get(SCENARIO_NUMBER_KEY);
     }
 
+    /**
+     * Sets the blockNumber.
+     * @param blockNumber the blockNumber
+     */
     public void setBlockNumber(int blockNumber) {
         if (blockNumber < 0) {
             throw new IllegalArgumentException("Scenario Number must be greater than 0");
@@ -55,6 +88,10 @@ public class ChoiceOptionImage {
         infos.put(BLOCK_NUMBER_KEY, String.valueOf(blockNumber));
     }
 
+    /**
+     * Sets the choiceOptionNumber.
+     * @param choiceOptionNumber the choiceOptionNumber
+     */
     public void setChoiceOptionNumber(int choiceOptionNumber) {
         if (choiceOptionNumber < 0) {
             throw new IllegalArgumentException("Scenario Number must be greater than 0");
@@ -62,6 +99,10 @@ public class ChoiceOptionImage {
         infos.put(CHOICE_OPTION_NUMBER_KEY, String.valueOf(choiceOptionNumber));
     }
 
+    /**
+     * Sets the scenarioNumber.
+     * @param scenarioNumber the scenarioNumber
+     */
     public void setScenarioNumber(int scenarioNumber) {
         if (scenarioNumber < 0) {
             throw new IllegalArgumentException("Scenario Number must be greater than 0");
@@ -69,6 +110,10 @@ public class ChoiceOptionImage {
         infos.put(SCENARIO_NUMBER_KEY, String.valueOf(scenarioNumber));
     }
 
+    /**
+     * Sets the title of the choiceOption.
+     * @param title the title.
+     */
     public void setTitle(String title) {
         Objects.requireNonNull(title);
         infos.put(TITLE_KEY, title);
